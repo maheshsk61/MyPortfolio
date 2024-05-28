c19fb3a42e7e55921334305d31dd5fa1f2c2ee82
maheshsk61 <maheshs61@gmail.com>
2024-05-28 19:54:21 +0530

    changed resume

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -3,7 +3,7 @@ export default function Resume() {
     return (
         <div className="Resume" id='resume'>
             <h1 className='text-info'>Resume</h1>
-            <a href="https://drive.google.com/file/d/1ygj29cZmsBYrL0zZITV_JTFS0bVg0O5_/view?usp=drive_link" style={{ textDecoration: 'none' }} target='blank'>Download Resume</a>
+            <a href="https://drive.google.com/file/d/1BKxX_PM6alaaHvkmghAWGrTB9UuLC4-a/view?usp=drive_link" style={{ textDecoration: 'none' }} target='blank'>Download Resume</a>
         </div>
     )
 }
